195cd5a65748eb8340486a26c14ffcadf6dad386
Chino Nanami <tino_nanami@7mi.site>
2025-11-04 21:43:05 +0900

    Linux環境動作改善

--- src/main/java/net/nicovrc/dev/Main.java
+++ src/main/java/net/nicovrc/dev/Main.java
@@ -333,36 +333,36 @@ public class Main {
 
         if (Function.config.getLogFolderPass().isEmpty()){
 
-            if (Function.ntSystem != null){
-                //UnixSystem unixSystem = new UnixSystem();
-
-            } else {
-                try {
-                    if (Function.config.isDebugOutput()){
-                        System.out.println("[Info] ログフォルダの自動取得");
-                    }
-                    String LocalAppData = System.getenv().get("LOCALAPPDATA");
+            try {
+                if (Function.config.isDebugOutput()){
+                    System.out.println("[Info] ログフォルダの自動取得");
+                }
+                String LocalAppData = System.getenv().get("LOCALAPPDATA");
+                if (Function.ntSystem != null){
                     file = new File("C:\\Users\\"+Function.ntSystem.getName()+"\\AppData\\LocalLow\\VRChat\\VRChat");
-                    //file = new File(LocalAppData+"Low\\VRChat\\VRChat");
-                    if (file.exists()) {
-                        Function.config.setLogFolderPass(file.getCanonicalPath());
-
-                        if (Function.config.isDebugOutput()) {
-                            System.out.println("[Info] 自動取得成功 : " + file.getCanonicalPath());
-                        }
-                    } else if (new File(LocalAppData+"Low\\VRChat\\VRChat").exists()){
-                        file = new File(LocalAppData+"Low\\VRChat\\VRChat");
-
-                        Function.config.setLogFolderPass(file.getCanonicalPath());
-                        System.out.println("[Info] 自動取得成功 : " + file.getCanonicalPath());
-                    } else {
-                        System.out.println("[Info] 自動取得失敗");
-                    }
-                } catch (Exception e) {
-                    //e.printStackTrace();
+                } else if (Function.unixSystem != null) {
+                    file = new File("/home/"+Function.unixSystem.getUsername()+"/.steam/steam/steamapps/compatdata/438100/pfx/drive_c/users/steamuser/AppData/LocalLow/VRChat/VRChat");
                 }
 
+                if (file.exists()) {
+                    Function.config.setLogFolderPass(file.getCanonicalPath());
+
+                    if (Function.config.isDebugOutput()) {
+                        System.out.println("[Info] 自動取得成功 : " + file.getCanonicalPath());
+                    }
+                } else if (Function.ntSystem != null && new File(LocalAppData+"Low\\VRChat\\VRChat").exists()){
+                    file = new File(LocalAppData+"Low\\VRChat\\VRChat");
+
+                    Function.config.setLogFolderPass(file.getCanonicalPath());
+                    System.out.println("[Info] 自動取得成功 : " + file.getCanonicalPath());
+                } else {
+                    System.out.println("[Info] 自動取得失敗");
+                }
+            } catch (Exception e) {
+                //e.printStackTrace();
             }
+
+
         }
 
         if (Function.config.isDebugOutput()){
