1ab41f50b972dbd147e25370fede95aadadeab46
Chino Nanami <tino_nanami@7mi.site>
2026-04-22 21:44:23 +0900

    update

--- run-batch/start.bat
+++ run-batch/start.bat
@@ -1,6 +1,7 @@
 @echo off
 
 if exist ".\start.ps1" (
+    rem found
 ) else (
     echo "Remove-Item ./start.ps1" >> start.ps1
     echo "Invoke-WebRequest -Uri https://raw.githubusercontent.com/nicovrc-net/VRCVideoLogViewer/refs/heads/release/run-batch/start.ps1 -OutFile ./start.ps1" >> start.ps1
